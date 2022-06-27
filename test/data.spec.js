0d95cb567a28e45a254661a5be0b3c57f9b04fd3
GabrielaC3 <gabycr.3.2.gcr@gmail.com>
2022-06-27 18:32:30 -0500

    Buscador en H y P

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,4 +1,4 @@
-import {sortData,filterCasa,filterEspecie,filterGenero,filterAscendencia} from '../src/data.js';
+import {sortData,filterCasa,filterEspecie,filterGenero,filterAscendencia,computeStats} from '../src/data.js';
 import data from '../src/data/harrypotter/data.js';
 
 
@@ -6,11 +6,17 @@ describe('sorData', () => {
   it('Deberia ser una función', () => {
     expect(typeof sortData).toBe('function');
   });
-
-  it('Deberia retornar [24, 4, 37, 14, 30, 32, 44, 25, 26, 34, 47, 33, 5, 13, 40, 27, 22, 46, 45, 23, 8, 9, 0, 2, 38, 41, 10, 35, 6, 15, 39, 20, 36, 18, 12, 17, 29, 1, 21, 3, 42, 7, 16, 19, 31, 11, 28, 43] para "personajes ordenado ascendente"', () => {
-    
-    expect(sortData(data.characters,1)).toStrictEqual([24, 4, 37, 14, 30, 32, 44, 25, 26, 34, 47, 33, 5, 13, 40, 27, 22, 46, 45, 23, 8, 9, 0, 2, 38, 41, 10, 35, 6, 15, 39, 20, 36, 18, 12, 17, 29, 1, 21, 3, 42, 7, 16, 19, 31, 11, 28, 43]);
+  it('Debería devolver lo mismo', () => {
+    expect(sortData(data.characters,0)).toStrictEqual([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47])
   });
+  it('Deberia retornar los personajes ordenado de manera ascendente"', () => {
+    expect(sortData(data.characters,1)).toStrictEqual([24,4,37,14,30,32,44,25,26,34,47,33,5,13,40,27,22,46,45,23,8,9,0,2,38,41,10,35,6,15,39,20,36,18,12,17,29,1,21,3,42,7,16,19,31,11,28,43]);
+  });
+
+  it('Deberia retornar los personajes ordenado de manera descendente"', () => {
+    expect(sortData(data.characters,2)).toStrictEqual([43,28,11,31,19,16,7,42,3,21,1,29,17,12,18,36,20,39,15,6,35,10,41,38,2,0,9,8,23,45,46,22,27,40,13,5,33,47,34,26,25,44,32,30,14,37,4,24]);
+  });
+
 });
 
 describe('filterCasa', ()=>{
@@ -66,6 +72,15 @@ describe('filterAscendencia', ()=>{
     expect(filterAscendencia(data.characters,'Squib')).toStrictEqual([37]);
   });
 
+});
+
+describe('computeStats', ()=>{
+  it('Deberia ser una función',()=>{
+    expect(typeof computeStats).toBe('function');
+  });
+  it('Deberia retornar el promedio',()=>{
+    expect(computeStats([5,5,5])).toStrictEqual("6.25");
+  });
 });
 
 
