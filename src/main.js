b1766e6626445be04da73c970918134254a8025f
LisethLira <liseth.lira.123@gmail.com>
2022-06-27 18:34:27 -0500

    responsive

--- src/main.js
+++ src/main.js
@@ -288,10 +288,10 @@ menuEspecie.addEventListener('mouseleave',()=>{
     menuAsc.style.display="none";
     menuBuscador.style.display="none"
     menuOrden.style.display="none";
-   })
+   });
   menuGenero.addEventListener('mouseleave',()=>{
      menuGenero.style.display="none";
-    })
+    });
 
   labelAscende.addEventListener('mouseenter',()=>{
     menuAsc.style.display="block";
@@ -300,10 +300,10 @@ menuEspecie.addEventListener('mouseleave',()=>{
     menuGenero.style.display="none";
     menuBuscador.style.display="none"
     menuOrden.style.display="none";
-     })
+     });
   menuAsc.addEventListener('mouseleave',()=>{
     menuAsc.style.display="none";
-      })
+      });
 
   labelBuscador.addEventListener('mouseenter',()=>{
     menuBuscador.style.display="block"
@@ -312,7 +312,7 @@ menuEspecie.addEventListener('mouseleave',()=>{
     menuEspecie.style.display="none";
     menuGenero.style.display="none";
     menuOrden.style.display="none";
-  })
+  });
 
   menuBuscador.addEventListener('mouseleave',()=>{
     menuBuscador.style.display="none";
